57352c07a5de04cb3f951ac092fb282d3e8a0ca0
CYADEM <notwinner420@gmail.com>
2025-07-11 19:24:42 -0300

    files edited

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.cuentas',
+    'cuentas',
     'fichas',
     'noticias',
     'noticias_portadas',
