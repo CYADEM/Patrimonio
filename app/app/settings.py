fc78dc64084f77faab63ba5f7ae83e08f89b7c19
CYADEM <notwinner420@gmail.com>
2025-07-15 20:32:47 -0300

    header edited & another things

--- app/app/settings.py
+++ app/app/settings.py
@@ -110,7 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'app/static',  # Verifica que esta ruta sea correcta
 ]
