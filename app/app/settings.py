0653ab6c0912f008a7b82ba3661bc38995bf5398
CYADEM <notwinner420@gmail.com>
2025-07-16 01:46:23 -0300

    bg url edited

--- app/app/settings.py
+++ app/app/settings.py
@@ -109,8 +109,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
